9b7f4bf359e6d3120df9232a3d6d51e7754e2201
Eric Agol <agol@uw.edu>
2017-10-05 11:03:33 +0200

    deprecated ttv_derivative; added jacobian keplerij to ttv.jl

--- hernandez_version/kepler_step.jl
+++ hernandez_version/kepler_step.jl
@@ -4,7 +4,10 @@ include("kepler_solver_derivative.jl")
 # 
 function kepler_step!(gm::Float64,h::Float64,state0::Array{Float64,1},state::Array{Float64,1})
 # compute beta, r0, dr0dt, get x/v from state vector & call correct subroutine
-  x0=state0[2:4]
+x0 = zeros(3)
+for i=1:3
+  x0[i]=state0[1+i]
+end
   r0 = sqrt(x0[1]*x0[1]+x0[2]*x0[2]+x0[3]*x0[3])
   v0 = state0[5:7]
   dr0dt = (x0[1]*v0[1]+x0[2]*v0[2]+x0[3]*v0[3])/r0
